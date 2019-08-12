15c8da9ae2ef50ab0188dd7257548ac702c5305c
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-12 17:00:29 -0500

    Heroku postgresql db setup & devise basic setup

---
+++ db/migrate/20190812215303_adding_missing_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddingMissingFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :image_link, :string
+    add_column :users, :name, :string
+    add_column :users, :birth_day, :datetime
+    add_column :users, :bio, :text
+  end
+end
