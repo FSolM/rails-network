7af37579b73cd6e94f57a786dbb5aa8bfef7a5bb
Carlos Sol <fersolmarin@hotmail.com>
2019-08-13 16:00:12 -0500

    Posts Controller Generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
   get 'feed/index'
   devise_for :users, controllers: { confirmations: "user/confirmations",
                                     passwords: "user/passwords",
