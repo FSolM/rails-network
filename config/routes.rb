7734b9f1dbd6a68f9cc8f2ca3b695a3edb94593e
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-13 15:36:49 -0500

    Devise user views & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'user#new'
+  devise_for :users, controllers: { confirmations: "user/confirmations",
+                                    passwords: "user/passwords",
+                                    registrations: "user/registrations",
+                                    sessions: "user/sessions",
+                                    unlocks: "user/unlocks",
+                                    omniauth_callbacks: "user/omniauth_callbacks",
+                                  }
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
