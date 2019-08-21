f2fb53ad7223fa14bea0ce9c3370348b8a8921dc
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-21 13:36:07 -0500

    Edit post styling

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,7 @@ class FeedController < ApplicationController
   def index
     # Calls Current User And Can Make Posts
     @user = current_user
-    @user_friends = User.first(4)
+    @user_friends = User.last(4)
     @recent_posts = Post.order(created_at: :desc)
     @new_post = Post.new()
     puts current_user.name
