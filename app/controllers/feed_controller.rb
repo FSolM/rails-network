ed17f82575759499eba166a287b8257ea94ca518
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-28 11:30:52 -0500

    Added default scope for sorting the posts

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,7 +3,7 @@ class FeedController < ApplicationController
     # Calls Current User And Can Make Posts
     @user = current_user
     @user_friends = User.last(4)
-    @recent_posts = Post.order(created_at: :desc)
+    @recent_posts = Post
     @new_post = Post.new()
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  default_scope { order(created_at: :desc) }
+  
   validates :author, presence: true
   validates :content, presence: true
   
