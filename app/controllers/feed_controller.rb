953f1a8af18ceb21c5e2626a5319a211a813977a
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-28 13:56:17 -0500

    Replaced redundant @user with current_user instead

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,9 +1,9 @@
 class FeedController < ApplicationController
   def index
     # Calls Current User And Can Make Posts
-    @user = current_user
-    @user_friends = User.last(4)
-    @recent_posts = Post
+
+    @user_friends = User.last(4)  # Friendships not implemented yet (show 4 random users)
+    @recent_posts = Post.all
     @new_post = Post.new()
   end
 end
