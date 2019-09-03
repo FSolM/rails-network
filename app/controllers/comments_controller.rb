3d2ce6c77c810381d56966238d6247653e6a80eb
Carlos Sol <fersolmarin@hotmail.com>
2019-09-03 17:34:12 -0500

    Comment's #edit Implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   before_action :search_comment, only: %i[destroy update]
 
   def search_comment
-    @comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:post_id])
   end
 
   def new
@@ -21,6 +21,24 @@ class CommentsController < ApplicationController
     redirect_to post_path(Post.find(params[:post_id]))
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:post_id])
+  end
+
+  def update
+    if @comment.author == current_user
+      if @comment.update(patch_params)
+        flash[:notice] = "You have successfully edited a comment"
+      else
+        flash[:alert] = "There has been an error when editing your comment, please try again later"
+      end
+    else
+      flash[:alert] = "You can't edit a comment that you don't own, please login"
+    end
+    redirect_to post_path(Post.find(params[:id]))
+  end
+
   def destroy
     if @comment.author == current_user
       if @comment.destroy
@@ -34,25 +52,12 @@ class CommentsController < ApplicationController
     redirect_to post_path(Post.find(params[:post_id]))
   end
 
-  def update
-    if @comment.author == current_user
-      if @comment.update(patch_params)
-        flash[:notice] = "You have successfully edited a comment"
-      else
-        flash[:alert] = "There has been an error when editing your comment, please try again later"
-      end
-    else
-      flash[:alert] = "You can't edit a comment that you don't own, please login"
-    end
-    redirect_to feed_path
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
   end
 
-  private
-    def comment_params
-      params.require(:comment).permit(:content, :post_id)
-    end
-
-    def patch_params
-      params.require(:patch).permit(:content)
-    end
+  def patch_params
+    params.require(:patch).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :posts, only: [:new, :create, :show, :destroy, :edit, :update] do
-    resources :comments, only: [:create, :destroy, :update]
+    resources :comments, only: [:new, :create, :destroy, :edit, :update]
   end
   resources :reactions, only: [:create, :destroy, :update]
 
