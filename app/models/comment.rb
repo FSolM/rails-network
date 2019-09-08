1bf7866eeb9ead2ca2f244cfdd1b344f870adfd8
Carlos Sol <fersolmarin@hotmail.com>
2019-09-08 15:54:18 -0500

    Model Attributes Refactored

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,6 @@
 class Comment < ApplicationRecord
-  validates :author, presence: true
-  validates :post, presence: :true
   attr_readonly :user_id, :post_id
 
   belongs_to :author, foreign_key: :author_id, class_name: :User
-  belongs_to :post, foreign_key: :post_id, class_name: :Post
+  belongs_to :post
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,6 +1,7 @@
 class Reaction < ApplicationRecord
   validates :user_id, uniqueness: { scope: :post_id } 
   validates :reaction_type, presence: true
+  
   attr_readonly :user, :post, :user_id, :post_id
 
   belongs_to :user
