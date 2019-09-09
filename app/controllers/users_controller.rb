622fecaa4ec938ac0b8851433bfc061e06b4a4f2
Carlos Sol <fersolmarin@hotmail.com>
2019-09-09 13:33:16 -0500

    User's View Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_friends = User.last(4)
-    @recent_posts = @user.authored_posts
+    @recent_posts = @user.authored_posts.first(5)
   end
 end
