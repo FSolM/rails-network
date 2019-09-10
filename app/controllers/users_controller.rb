9936f629d3b0a463b2d5cefc087570e081965b83
Carlos Sol <fersolmarin@hotmail.com>
2019-09-10 14:08:10 -0500

    Profile Update Functionality Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user_friends = User.last(4)
     @recent_posts = @user.authored_posts.first(5)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if User.find(params[:id]).update(user_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to user_path(current_user)
+    else
+      flash.now[:alert] = "There has been an error while editing your profile, please try again later"
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :bio)
+  end
 end
