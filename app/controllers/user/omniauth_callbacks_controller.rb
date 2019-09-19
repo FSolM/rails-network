aca54cbf56d5c96b61be1237763da96b295870ad
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-09-19 13:18:40 -0500

    removed commented code

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -17,16 +17,6 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
   protected
 
   def failure
